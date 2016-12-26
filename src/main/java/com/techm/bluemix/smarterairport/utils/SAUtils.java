3f0213ccf9ae4c07735241b47a84866c72439fc4
Santhosh Raj Radhakrishnan <SR00487261@TechMahindra.com>
2016-12-26 06:30:47 +0000

    fixed url

--- src/main/java/com/techm/bluemix/smarterairport/utils/SAUtils.java
+++ src/main/java/com/techm/bluemix/smarterairport/utils/SAUtils.java
@@ -19,45 +19,7 @@ import com.ibm.nosql.json.util.JSON;
 
 public class SAUtils {
 	
-	//Service call within Bluemix
-	private String databaseHost;
-	private int port;
-	private String user;
-	private String password;
-	private String url;
-		private boolean processVCAP() {
-		// VCAP_SERVICES is a system environment variable
-		// Parse it to obtain the for DB2 connection info
-		String VCAP_SERVICES = System.getenv("VCAP_SERVICES");
-		writer.println("Key is: " + VCAP_SERVICES);
-		if (VCAP_SERVICES != null) {
-			// parse the VCAP JSON structure
-			BasicDBObject obj = (BasicDBObject) JSON.parse(VCAP_SERVICES);
-			String thekey = null;
-			Set<String> keys = obj.keySet();
-			// Look for the VCAP key that holds the SQLDB information
-			for (String eachkey : keys) {
-				writer.println("Key is: " + eachkey);
-				// Just in case the service name gets changed to lower case in the future, use toUpperCase
-				if (eachkey.toUpperCase().contains("weatherinsights")) {
-					thekey = eachkey;
-				}
-			}
-			BasicDBList list = (BasicDBList) obj.get(thekey);
-			obj = (BasicDBObject) list.get("0");
-			
-			// parse all the credentials from the vcap env variable
-			obj = (BasicDBObject) obj.get("credentials");
-			databaseHost = (String) obj.get("host");
-			databaseName = (String) obj.get("db");
-			port = (int)obj.get("port");
-			user = (String) obj.get("username");
-			password = (String) obj.get("password");
-			url = (String) obj.get("jdbcurl");
-			writer.println("Service found: " + url);
-		}	
-		return true;
-	}
+
 	
 	public static Map<String, String> flightCodeMap;
 	public static Map<String, String> countryCodeMap;
