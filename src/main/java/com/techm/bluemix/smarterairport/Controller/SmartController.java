6d0838eda1fa14d514ea1fbc9141c7a2621b5b10
Santhosh Raj Radhakrishnan <SR00487261@TechMahindra.com>
2017-01-18 18:59:12 +0000

    /

--- src/main/java/com/techm/bluemix/smarterairport/Controller/SmartController.java
+++ src/main/java/com/techm/bluemix/smarterairport/Controller/SmartController.java
@@ -30,17 +30,17 @@ public class SmartController {
 		String application_id=api+".appid";
 		String application_key=api+".appkey";
 		Properties prop = new Properties();
-<<<<<<< HEAD
+
 		OutputStream output = null;
 		output = new FileOutputStream("/db.properties");
-=======
+
 		//OutputStream output = null;
 		//output = new FileOutputStream("db.properties");
->>>>>>> 2bd1caa1fc4f3a213f86b5dfcb74451cd356d4ac
+
 		prop.setProperty(application_id, appid);
 		prop.setProperty(application_key, appkey);
-		prop.store(new FileWriter("db.properties"),"API Key details");
-		//output.close();
+		prop.store(output,"API Key details");
+		output.close();
 		return new ModelAndView("home");		
 	}
 	
