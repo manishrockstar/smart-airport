3b5e1447e459351972cf1d692495d21d58db701e
Santhosh Raj Radhakrishnan <SR00487261@TechMahindra.com>
2017-01-18 13:48:24 +0000

    param

--- src/main/java/com/techm/bluemix/smarterairport/Controller/SmartController.java
+++ src/main/java/com/techm/bluemix/smarterairport/Controller/SmartController.java
@@ -8,6 +8,7 @@ import java.io.FileOutputStream;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.ui.Model;
 import com.techm.bluemix.smarterairport.Wrapper.LoginForm;
@@ -24,7 +25,7 @@ public class SmartController {
 	
 	// Update API ID and key
 	@RequestMapping(value="update", method={RequestMethod.GET,RequestMethod.POST})
-	public ModelAndView smarUpdate(@RequestParam("appid") String appid, @RequestParam("appkey") String appkey, @RequestParam("api") String api) throws IOException{
+	public ModelAndView smartUpdate(@RequestParam("appid") String appid,@RequestParam("appkey") String appkey,@RequestParam("api") String api) throws IOException{
 		String application_id=api+".appid";
 		String application_key=api+".appkey";
 		Properties prop = new Properties();
